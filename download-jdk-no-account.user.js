f8a90faad5d18ca8803d7dcab6157741ad7f3069
Name <marwanazuz@gmail.com>
2020-06-09 00:14:27 +0200

    Update download-jdk-no-account.user.js

--- download-jdk-no-account.user.js
+++ download-jdk-no-account.user.js
@@ -1,1 +1,25 @@
+// ==UserScript==
+// @name         Download JDK without account
+// @run-at       document-end
+// @version      0.1
+// @description  Download Java developer files without needing to login!
+// @author       Maeeen
+// @repo         https://github.com/Maeeen/userscripts-collection
+// @downloadURL  https://raw.githubusercontent.com/Maeeen/userscripts-collection/master/download-jdk-no-account.user.js
+// @grant        none
+// @match        https://www.oracle.com/*
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    document.querySelectorAll('a.license-link').forEach(anchor => {
+        const attr = anchor.getAttribute('data-file')
+        if (!attr)
+            return
+
+        anchor.addEventListener('click', () => {
+            window.open(attr.replace('otn', 'otn-pub'))
+        })
+    })
+})();
